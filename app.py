e5737fd9ee974a9aef83449474f690a18c8fa4de
Karan Kumar Singh <skarankumar690@gmail.com>
2023-12-28 09:59:41 +0530

    app.py

---
+++ app.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import easyocr
+from streamlit_cropperjs import st_cropperjs
+import cv2
+import imageio.v3 as iio
+
+reader = easyocr.Reader(['en'])
+st.header('Image to Text Converter 🔠')
+img = st.file_uploader('Upoad Image Here!!', type=['png', 'jpg', 'jpeg'], key='uploaded_pic')
+if img:
+    img = img.read()
+    cropped_pic = st_cropperjs(pic=img, btn_text="Submit!", key="foo")
+    if cropped_pic:
+        cropped_pic = iio.imread(cropped_pic)
+        cropped_pic = cv2.convertScaleAbs(cropped_pic, alpha = 0.8, beta = -25)
+        st.image(cropped_pic)
+
+if st.button('Show Output'):
+    result = reader.readtext(cropped_pic, detail=0, paragraph=True)
+    st.success(result)
